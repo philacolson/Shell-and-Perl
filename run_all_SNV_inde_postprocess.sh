09130852cc7256ec75882a220b01f5a7a2a35cb7
philacolson <philacolson@gmail.com>
2013-01-17 13:41:35 -0500

    Update run_all_SNV_inde_postprocess.sh

--- run_all_SNV_inde_postprocess.sh
+++ run_all_SNV_inde_postprocess.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-INPUT_SAMPLE_LIST=$1 #list of chromosomes to test
-DISEASE_CODE=$2
+INPUT_SAMPLE=$1 #list of chromosomes to test
+D_BAM_FILE=$2
+G_BAM_FILE=$3
 ##BAM_DIR=/nfs_exports/genomes/1/PCGP/BucketRaw/${DISEASE_CODE}
-BAM_DIR=/user/songliu/u2/group/Qiang/Exome/bladder_output/${DISEASE_CODE}
-DEBUG_LEVEL=$3
+#BAM_DIR=/user/songliu/u2/group/Qiang/Exome/bladder_output/${DISEASE_CODE}
+BAM_DIR=$4
+DEBUG_LEVEL=$5
 #Debug level can be 1-3, increasing with verbosity.  As a general rule, the grades pertain to the following: 1 just logs fatal events and ones that otherwise prematurely end the code, 2 mentions anything that deviates from expected code/error handler code is run, or other noteworthly events.  3 logs variable names before and after programs are run, and upon initialization.  Higher numbers post their description + any number's description lower than their own.  For example, a debug level of 2 would post every log statement that is not a level 3 log event.  It would post 1's, and 2's.
 
 ##SCRIPT_DIR=/nfs_exports/apps/internal/scripts
@@ -14,19 +16,22 @@ FILTER_DBSNP=$SCRIPT_DIR/filter_low_quality_dbSNP_4_tier1
 
 
 ##EXCEL_ROOT_DIR=/nfs_exports/linux-file1/home/naevegrp/jzhang2/NextGen/PCGP
-EXCEL_ROOT_DIR=/user/songliu/u2/group/Qiang/Exome/bladder_output/EXCEL
-LOG_DIR = EXCEL_ROOT_DIR
-
-echo $DEBUG_LEVEL >> LOG_DIR/debug.lvl
+EXCEL_ROOT_DIR=$6
+HIGH_LIST=$7
+LOW_LIST=$8
+GERM_LIST=$9
+LOG=${BAM_DIR}/${INPUT_SAMPLE}_LOG.txt
+echo $DEBUG_LEVEL >> $LOG
 `cat $LOG`
-for i in `cat ${INPUT_SAMPLE_LIST}`; do
-  if test ! -d ${EXCEL_ROOT_DIR}/${i} #if there is no directory for the SNP
+#reimplement below if we want a list of them to be entered
+#for i in `cat ${INPUT_SAMPLE}`; do
+  if test ! -d ${EXCEL_ROOT_DIR}/${INPUT_SAMPLE} #if there is no directory for the SNP
   then
 	if [ $DEBUG_LEVEL -gt 1 ]
 	then 
-  echo "INFO: no dir for ${EXCEL_ROOT_DIR}/${i}, making directory." >> $LOG
+  echo "INFO: no dir for ${EXCEL_ROOT_DIR}/${INPUT_SAMPLE}, making directory." >> $LOG
 	fi
-    mkdir -p ${EXCEL_ROOT_DIR}/${i}
+    mkdir -p ${EXCEL_ROOT_DIR}/${INPUT_SAMPLE}
     #exit 1
   fi
 done
@@ -36,27 +41,27 @@ then
 echo "DEBUG: created EXCEL directories." >> $LOG
 fi
 
-for i in `cat ${INPUT_SAMPLE_LIST}`; do
+for i in `cat ${INPUT_SAMPLE}`; do
   ##echo $i
-  cp ../${i}_G_bam.out .
-  cp ../${i}_bam_low.out ${i}_bam_low.out
-  cp ../${i}_bam_high_20.out ${i}_bam_high_20.out
+  cp ../${INPUT_SAMPLE}_G_bam.out .
+  cp ../${INPUT_SAMPLE}_bam_low.out ${INPUT_SAMPLE}_bam_low.out
+  cp ../${INPUT_SAMPLE}_bam_high_20.out ${INPUT_SAMPLE}_bam_high_20.out
 if [ $DEBUG_LEVEL -gt 2 ]
-	then echo "DEBUG:  ${SCRIPT_DIR}/run_somatic_mutation_analysis ${i} no_false_snp" >> $LOG
+	then echo "DEBUG:  ${SCRIPT_DIR}/run_somatic_mutation_analysis ${INPUT_SAMPLE} no_false_snp" >> $LOG
 	fi
   #hq
-  D_BAM=`ls ${BAM_DIR}/${i}_D*.bam`
-  G_BAM=`ls ${BAM_DIR}/${i}_G*.bam`
-  ${SCRIPT_DIR}/run_somatic_mutation_analysis ${i} no_false_snp ${D_BAM} ${G_BAM} ${LOG_DIR}
+  #D_BAM=`ls ${BAM_DIR}/${INPUT_SAMPLE}_D*.bam`
+  #G_BAM=`ls ${BAM_DIR}/${INPUT_SAMPLE}_G*.bam`
+  ${SCRIPT_DIR}/run_somatic_mutation_analysis ${i} no_false_snp ${D_BAM_FILE} ${G_BAM_FILE} ${DEBUG_LEVEL} ${LOG}
 done
 if [ $DEBUG_LEVEL -gt 2 ]
 	then
-echo "DEBUG:  ${SCRIPT_DIR}/run_check_snv_script ${INPUT_SAMPLE_LIST} ${DISEASE_CODE}" >> $LOG
+echo "DEBUG:  ${SCRIPT_DIR}/run_check_snv_script ${INPUT_SAMPLE} ${DISEASE_CODE}" >> $LOG
 fi
 
-${SCRIPT_DIR}/run_check_snv_script ${INPUT_SAMPLE_LIST} ${DISEASE_CODE}
+${SCRIPT_DIR}/run_check_snv_script ${INPUT_SAMPLE} ${DISEASE_CODE}
 
-for i in `cat ${INPUT_SAMPLE_LIST}`; do
+for i in `cat ${INPUT_SAMPLE}`; do
   echo $i
   SAMPLE_NAME=$i
 if [ $DEBUG_LEVEL -gt 2 ]
